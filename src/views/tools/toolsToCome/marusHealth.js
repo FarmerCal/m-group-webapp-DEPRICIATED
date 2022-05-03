32205b332f9ba189ddd76d930d73034d59ffebc4
cal <caldayham@gmail.com>
2022-05-03 17:58:55 -0500

    added header and mobile responsive drawer, installed mui and styled header

---
+++ src/views/tools/toolsToCome/marusHealth.js
@@ -0,0 +1,3 @@
+// marus health is a tool for keeping the human body healthy, that neccesitates a wholistic approach. Medication and surgery are one part but also proper nutrition, exercise,
+// and mindset. Health is so many other things as well, we will cover it all
+
